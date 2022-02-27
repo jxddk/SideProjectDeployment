44f3285aa82844b4df4b8023107623eae454a6d2
Joe Dean <joe@joedean.dev>
2022-02-27 22:46:33 +0100

    Updated to include environment variables in NGINX configuration

--- default/nginx/nginx.sh
+++ default/nginx/nginx.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+# Replacement of environment variables in config files
+# https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration-new-in-119
+# The script "20-envsubst-on-templates.sh" doesn't work exactly as needed, so
+# it is re-implemented here:
+files=$(find /deployment/nginx/templates -name "*.nginx" -type f)
+for file in $files
+do
+  vars="$(grep -oE '\$\{.+}' $file | uniq)"
+  for var in $vars
+  do
+    name=$(echo $var | grep -o -E '([A-Za-z]|[0-9]|_)+')
+    eval value=\${$name}
+    if [ "$value" = "" ]
+    then
+      echo UNDEFINED VARIABLE IN TEMPLATE $(basename $file): $var
+      exit 1
+    fi
+  done
+  envsubst $vars < $file | tee /deployment/nginx/$(basename $file)
+done
+
 # make some self-signed certs for served domains - these can be replaced by
 # certbot later. nginx can't start if it points to non-existent cert files!
 # make sure that openssl saves the keys to the same path and filename as certbot
 mkdir -p /etc/nginx/ssl/live/localhost/
 
 # selects domain names based on NGINX configuration, wherever an SSL key is defined
-for domain in "$(grep -Erhiwo "/etc/nginx/ssl/live/(.*)/privkey.pem" "/deployment/nginx" "/etc/nginx/nginx.conf" | cut -d "/" -f 6 | sort | uniq)"; do
+for domain in "$(grep -Erhiwo "/etc/nginx/ssl/live/(([A-Za-z]|[0-9]|_|-|\.)+)/privkey.pem" "/deployment/nginx" "/etc/nginx/nginx.conf" | cut -d "/" -f 6 | sort | uniq)"; do
   domains="${domains:+$domains}$domain"
 done
 echo Found domains: $domains
